c76de88525748a83d4591ee6c13108dda2d45816
Doruk <dorukyogscast@gmail.com>
2022-06-07 21:37:24 +0300

    rewrite

--- ts/Artifact.ts
+++ ts/Artifact.ts
@@ -1,282 +1,154 @@
-// Base Artifact Class/Module - Abstract
-class Game {
-    readonly LaneAmount: number; // default is 3
-    readonly LaneSize: number; // 5
-    readonly HandLimit: number; // 12
-    readonly DeckHeroAmount: number; // 5
-    readonly DeckCastableAmount: number; // 40
-    readonly DeckItemAmount: number; // 10
-    readonly TowerHealth: number; // 30
-    players: Array<Player>;
-    spectators: Array<Spectator> = new Array<Spectator>();
-
-    constructor(
-        LaneAmount: number, LaneSize: number, DeckLimit: number,
-        TowerHealth: number, PlayerCount: number, DeckHeroAmount: number,
-        DeckCastableAmount: number, DeckItemAmount: number
-    ) {
-        this.LaneAmount = LaneAmount;
-        this.LaneSize = LaneSize;
-        this.HandLimit = DeckLimit;
-        this.TowerHealth = TowerHealth;
-        this.DeckHeroAmount = DeckHeroAmount;
-        this.DeckCastableAmount = DeckCastableAmount;
-        this.DeckItemAmount = DeckItemAmount;
-        this.players = new Array<Player>(PlayerCount);
-    }
-
-    setPlayer(index: number, name: string, deck: Deck): boolean {
-        if (this.players[index])
-            return false;
-        else {
-            this.players[index] = new Player(name, index, this, deck);
-            return true;
-        }
-    }
-
-    addSpectator(name: string): boolean {
-        this.spectators.push(new Spectator(name, this.spectators.length));
-        return true;
+interface Ruleset {
+    Lane: {
+        amount: number,             // default is 3
+        size: number                // 5
+    },
+    Hand: {
+        limit: number,              // 12
+    },
+    Deck: {
+        heroAmount: number,         // 5
+        castableAmount: number,     // 40
+        itemAmount: number          // 10
+    },
+    Tower: {
+        health: number              // 30
+    },
+    Player: {
+        amount: number              // 2
     }
 }
 
-class User {
-    name: string;
+const FoundryRules: Ruleset = { //Artifact 2 Ruleset
+    Lane: {
+        amount: 3,
+        size: 5
+    },
+    Hand: {
+        limit: 12,
+    },
+    Deck: {
+        heroAmount: 5,
+        castableAmount: 40,
+        itemAmount: 10
+    },
+    Tower: {
+        health: 30
+    },
+    Player: {
+        amount: 2
+    }
+}
+
+
+abstract class GameObject<Ptype, Ctype> {
+    static #currentId = 1;
     id: number;
-    constructor(Name: string, Id: number) {
-        this.name = Name;
-        this.id = Id;
+    parent?: Ptype;
+    child?: Ctype;
+    constructor(hierarchy?: { parent?: Ptype, child?: Ctype }) {
+        this.id = GameObject.#currentId++;
+        this.parent = hierarchy?.parent;
+        this.child = hierarchy?.child;
     }
 }
 
-class Spectator extends User { }
-
-class Player extends User {
-    readonly game: Game;
-    lanes: Array<Lane>;
-    readonly deck: PlayDeck;
-    constructor(Name: string, Id: number, game: Game, deck: Deck) {
-        super(Name, Id);
-        this.game = game;
-        this.lanes = new Array<Lane>(game.LaneAmount);
-        for (let i = 0; i < game.LaneAmount; i++)
-            this.lanes[i] = new Lane(this);
-        this.deck = new PlayDeck(game, deck).assign(this);
+abstract class Game {
+    static Players: Array<Player> = new Array<Player>;
+    static Ruleset: Ruleset;
+    static Board: Board;
+    static setRuleset(Ruleset: Ruleset) {
+        Game.Ruleset = Ruleset;
     }
-
-    LaneAmount(): number {
-        return this.lanes.length;
+    static addPlayer(Player: Player) {
+        Game.Players.push(Player);
     }
 }
+
+interface Color {
+    name: string,
+    cssVar: string,
+    creepCssVar?: string
+}
+
+type GameObjectContainer = Array<GameObject<any, any>>;
 
 class Deck {
-    heroes: Array<Hero | null>;
-    cards: Array<Creep | Spell | TowerEnchantment | null>;
-    items: Array<Item | null>;
-    player?: Player;
-    readonly game: Game;
-    constructor(game: Game) {
-        this.game = game;
-        this.heroes = new Array<null>(game.DeckHeroAmount);
-        this.cards = new Array<null>(game.DeckCastableAmount);
-        this.items = new Array<null>(game.DeckItemAmount);
-    }
-
-    pushCard(card: Creep | Spell | TowerEnchantment, times: number = 1): boolean {
-        let indArr: Array<number> = [];
-
-        for (let i = 0; times; i++)
-            if (!this.cards[i]) {
-                indArr.push(i);
-                times--;
-            }
-
-        if (!times) { //times == 0
-            let i;
-            while ((i = indArr.pop()) != undefined) {
-                this.cards[i] = card;
-                if (this.player)
-                    card.player = this.player;
-            }
-            return true;
-        }
-        else
-            return false;
-    }
-
-    pushHero(hero: Hero): boolean {
-        for (let i: number = 0; i < this.heroes.length; i++)
-            if (!this.heroes[i]) {
-                this.heroes[i] = hero;
-                if (this.player)
-                    hero.player = this.player;
-                this.pushCard(hero.signature, 3);
-                return true;
-            };
-
-        return false;
-    }
-
-    assign(player: Player): Deck {
-        this.player = player;
-        return this;
-    }
-}
-
-class PlayDeck extends Deck {
-    constructor(game: Game, deck: Deck) {
-        super(game);
-        this.heroes = deck.heroes.map(e => e); //clone of the array
-        this.cards = deck.cards.map(e => e);
-        this.items = deck.items.map(e => e);
-    }
 
 }
 
-class Hand {
-    readonly cards: Array<Card>;
-    constructor(DeckLimit: number) {
-        this.cards = new Array<Card>(DeckLimit);
-    }
+class Board extends GameObject<undefined, Lane>{
+
 }
 
-class Lane {
-    readonly cards: Array<Deployable | null>;
-    tower: Tower;
-    player: Player;
-    constructor(player: Player) {
-        this.player = player;
-        this.cards = new Array<null>(player.game.LaneSize);
-        this.tower = new Tower(player.game.TowerHealth);
-    }
+class Lane extends GameObject<Board, Side>{
 
-    deployCard(card: Deployable, at: number): boolean {
-        if (this.cards[at])
-            return false;
-        else {
-            this.cards[at] = card;
-            card.player = this.player;
-            card.lane = this;
-            card.isDeployed = true;
-            card.deployedAt = at;
-            return true;
-        }
-    }
-
-    removeCard(at: number): boolean {
-        let card = this.cards[at];
-        if (card instanceof Deployable) {
-            card.isDeployed = false;
-            this.cards[at] = null;
-            return true;
-        }
-        else
-            return false;
-    }
 }
 
-class Tower {
-    health: number;
-    constructor(Health: number) {
-        this.health = Health;
-    }
+class Side extends GameObject<Lane, GameObjectContainer>{
+
 }
 
-class Color {
-    readonly type: string;
-    constructor(Type: string) {
-        if (Type == "Red" || "Blue" || "Black" || "Green" || "Colourless")
-            this.type = Type;
-        else
-            this.type = "Colourless"; //Default Behaviour
-    }
-}
-
-abstract class Card {
+abstract class Card extends GameObject<Side, undefined> { //TODO: add a ability container as child
     name: string;
     color: Color;
-    player?: Player;
-    constructor(Name: string, color: string) {
+    constructor(Name: string, Color: Color) {
+        super();
         this.name = Name;
-        this.color = new Color(color);
+        this.color = Color;
     }
 }
 
-abstract class Deployable extends Card {
-    isDeployed: boolean = false;
-    deployedAt?: number;
-    lane?: Lane;
+class Player {
+    //activeDeck: Deck;
+    readonly passiveDeck: Deck;
+    name: string;
+    id: number;
+    hand?: Array<Card>;
 
-    deploy(lane: Lane, at: number): boolean {
-        return lane.deployCard(this, at);
+    constructor(Name: string, Id: number, Deck: Deck) {
+        this.name = Name;
+        this.id = Id;
+        this.passiveDeck = Deck;
     }
 
-    remove(): boolean {
-        if (this.lane && this.isDeployed && this.deployedAt)
-            return this.lane?.removeCard(this.deployedAt);
-        else
-            return false;
-    }
 }
 
 
 
-abstract class Castable extends Card {
 
+
+const Red: Color = {
+    name: "Red",
+    cssVar: "--cardred",
+    creepCssVar: "--creepred"
 }
 
-
-class Hero extends Deployable {
-    signature: Signature;
-    constructor(name: string, color: string, signature: Signature) {
-        super(name, color);
-        this.signature = signature;
-    }
+const Blue: Color = {
+    name: "Blue",
+    cssVar: "--cardblue",
+    creepCssVar: "--creepblue"
 }
 
-class Creep extends Deployable {
-
+const Green: Color = {
+    name: "Green",
+    cssVar: "--cardgreen",
+    creepCssVar: " --creepgreen"
 }
 
-class Spell extends Castable {
-
-    mana: number;
-    constructor(Name: string, Color: string, Mana: number) {
-        super(Name, Color);
-        this.mana = Mana;
-    }
+const Black: Color = {
+    name: "Black",
+    cssVar: "--cardblack",
+    creepCssVar: "--creepblack"
 }
 
-class TowerEnchantment extends Castable {
-
-    mana: number;
-    constructor(Name: string, Color: string, Mana: number) {
-        super(Name, Color);
-        this.mana = Mana;
-    }
+const Gold: Color = {
+    name: "Gold",
+    cssVar: "--cardgold"
 }
 
-class Signature extends Spell {
-
+const Gray: Color = {
+    name: "Gold",
+    cssVar: "--creepgray",
+    creepCssVar: "--creepgray"
 }
-
-class Item extends Card {
-    mana: number;
-    gold: number;
-    constructor(Name: string, Color: string, Mana: number, Gold: number) {
-        super(Name, Color);
-        this.mana = Mana;
-        this.gold = Gold;
-    }
-}
-
-class Effect {
-
-}
-
-class Ability {
-
-}
-
-
-
--- ts/main.ts
+++ ts/main.ts
@@ -1,29 +1,5 @@
 
 window.onload = () => {
-    let Artifact2 = new Game(3, 5, 12, 30, 2, 40, 10, 30);
-    let dorukDeck = new Deck(Artifact2);
-    let sig = new Signature("Culling Blade", "Red", 2);
-    let Axe = new Hero("Axe", "Red", sig);
-    dorukDeck.pushHero(Axe);
-    dorukDeck.cards.forEach(e => {
-        if (e)
-            console.log(e.name);
-    });
-    if (Artifact2.setPlayer(0, "Doruk", dorukDeck)) {
-        let ourAxe = Artifact2.players[0].deck.heroes[0];
-        if (ourAxe) {
-            console.log(Artifact2.players[0].name);
-
-            Artifact2.players[0].lanes[1].deployCard(ourAxe, 1);
-
-            console.log(ourAxe.player?.name);
-            console.log(ourAxe.isDeployed);
-
-            ourAxe.remove();
-
-            console.log(ourAxe.isDeployed);
-        }
-
-    }
+    Game.setRuleset(FoundryRules);
 
 }
