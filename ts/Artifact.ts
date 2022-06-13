0a6347c830867c87dda73d9bdc79491e61c5c0e5
Doruk <dorukyogscast@gmail.com>
2022-06-13 13:34:25 +0300

    new Addinations

--- ts/Artifact.ts
+++ ts/Artifact.ts
@@ -40,23 +40,22 @@ const FoundryRules: Ruleset = { //Artifact 2 Ruleset
     }
 }
 
-
-abstract class GameObject<Ptype, Ctype> {
+abstract class GameObject<Ptype> { //Ptype refers to parent type
     static #currentId = 1;
     id: number;
     parent?: Ptype;
-    child?: Ctype;
-    constructor(hierarchy?: { parent?: Ptype, child?: Ctype }) {
+    constructor(parent?: Ptype) {
         this.id = GameObject.#currentId++;
-        this.parent = hierarchy?.parent;
-        this.child = hierarchy?.child;
+        this.parent = parent;
     }
 }
 
 abstract class Game {
     static Players: Array<Player> = new Array<Player>;
     static Ruleset: Ruleset;
-    static Board: Board;
+
+    static Lanes: Array<Lane> = new Array<Lane>;
+
     static setRuleset(Ruleset: Ruleset) {
         Game.Ruleset = Ruleset;
     }
@@ -71,31 +70,75 @@ interface Color {
     creepCssVar?: string
 }
 
-type GameObjectContainer = Array<GameObject<any, any>>;
-
 class Deck {
 
 }
 
-class Board extends GameObject<undefined, Lane>{
+type Side = Array<Card>;
 
+class Lane extends GameObject<Game>{
+    sides: Array<Side>;
+    constructor() {
+        super(Game);
+        this.sides = new Array<Side>(Game.Ruleset.Player.amount);
+    }
 }
 
-class Lane extends GameObject<Board, Side>{
-
+interface CardInterface {
+    name: string,
+    color: Color,
+    armour: number,
+    attack: number,
+    health: number,
 }
 
-class Side extends GameObject<Lane, GameObjectContainer>{
-
-}
-
-abstract class Card extends GameObject<Side, undefined> { //TODO: add a ability container as child
+type CardParent = Lane | Player; //Parent of the Card
+type Points = { armour: number, attack: number, health: number };
+abstract class Card implements CardInterface { //TODO: add a ability container
     name: string;
     color: Color;
-    constructor(Name: string, Color: Color) {
-        super();
+    readonly armour: number;
+    readonly attack: number;
+    readonly health: number;
+
+    constructor(Name: string, Color: Color, Points: Points | undefined) {
         this.name = Name;
         this.color = Color;
+
+        if (Points) {
+            this.armour = Points.armour;
+            this.attack = Points.attack;
+            this.health = Points.health;
+        } else
+            this.armour = this.attack = this.health = 0;
+
+    }
+
+    newInstance(Parent: CardParent): Active { //gives a new card
+        return new Active(this, Parent);
+    }
+}
+
+class Active extends GameObject<CardParent> implements CardInterface {
+    readonly name: string;
+    readonly color: Color;
+
+    armour: number;
+    attack: number;
+    health: number;
+
+    constructor(Card: Card, Parent: CardParent) {
+        super(Parent);
+        this.color = Card.color;
+        this.name = Card.name;
+
+        this.armour = Card.armour;
+        this.attack = Card.attack;
+        this.health = Card.health;
+    }
+
+    setParent(Parent: CardParent) {
+        this.parent = Parent;
     }
 }
 
@@ -113,10 +156,6 @@ class Player {
     }
 
 }
-
-
-
-
 
 const Red: Color = {
     name: "Red",
